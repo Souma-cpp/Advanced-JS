6786b6093b67617afdfef833cf516aa3144b2a56
souma-js <soumadityadey9933@gmail.com>
2025-07-26 11:03:44 +0530

    added the es6 module export import portions of javascript

---
+++ ES6_and_commonJS.js
@@ -0,0 +1,14 @@
+export function greet() {
+  return "hello";
+}
+
+export function morning_greet() {
+  return "hello";
+}
+export function night_greet() {
+  return "hello";
+}
+
+export default function greetM() {
+  return "greetings";
+}
---
+++ testing.js
@@ -0,0 +1,5 @@
+// import { greet, morning_greet, night_greet } from "./ES6_and_commonJS.js";
+// console.log(greet());
+
+import greetM from "./ES6_and_commonJS.js";
+console.log(greetM());
