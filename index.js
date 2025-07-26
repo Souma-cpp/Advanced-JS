a28f63b7045d84eab04f9e6b578fa898f21371dd
souma-js <soumadityadey9933@gmail.com>
2025-07-26 09:17:01 +0530

    added the asyncronous nature of javascript

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log("hello world");
-
-setTimeout(function greet() {
+function greet() {
   console.log("hey hello from greet");
-}, 4000);
+}
+setTimeout(greet, 4000);
 
 for (let i = 1; i <= 10; ++i) {
   console.log(i);
